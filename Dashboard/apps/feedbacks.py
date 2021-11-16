61cffc7b257dd71f856492a414149043ac19d079
Jorge <salgadojorge@javeriana.edu.co>
2021-11-15 23:47:44 -0500

    limpieza en carpeta apps (previamente de importacion

--- Dashboard/apps/feedbacks.py
+++
@@ -1,6 +0,0 @@
-
-import pandas as pd
-print('Importando feedbacks')
-feedbacks: pd.DataFrame = pd.DataFrame(pd.read_json('apps/feedback_users.json'))
-print('Completado')
-
--- Dashboard/apps/recomendaciones_completas.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-print('Importando recomendaciones_completas')
-recomendaciones_completas: pd.DataFrame = pd.DataFrame(pd.read_json('apps/recomedaciones_completas.json'))
-print('Completado')
